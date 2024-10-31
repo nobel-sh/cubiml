65d2f5e96a649f26e048241937e6c9ca8f8ab9ba
nobel <nobel2073@gmail.com>
2024-10-31 17:17:02 +0545

    add LALRPOP grammar for generating ASTs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main(){
-    println!("cubiml");
+pub mod ast;
+
+use ast::*;
+
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(pub grammar);
+
+
+pub fn parse(source: &str) -> Result<Program, String> {
+    println!("{}", source);
+    let parsed = grammar::ScriptParser::new().parse(source).unwrap();
+    Ok(parsed)
+}
+
+fn main() {
+    parse("let x = true; let y = x ").unwrap();
 }
